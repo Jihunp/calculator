012fde507343672ed71fddf6818d64c7e4919111
jihunp <edwardpark.park@gmail.com>
2024-04-10 13:48:47 -0700

    made calculator do arithmetic

---
+++ app/javascript/controllers/my_calculator_controller.js
@@ -0,0 +1,30 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    console.log("connected");
+  }
+  initialize() {
+    this.value = '';
+  }
+
+  display() {
+    const screenElement = this.element.querySelector("#calculator-screen");
+
+    screenElement.innerHTML = this.value
+  }
+
+  append(event) {
+    const input = event.target.innerHTML;
+    this.value += input;
+
+    this.display()
+  }
+  equals() {
+    this.value = eval(this.value);
+
+    this.display();
+
+    this.value = ''
+  }
+}
